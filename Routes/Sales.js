5f7199b5cd011c71501293699fafe73530d9b254
KshitizAgarwal21 <kshitizagarwal27@gmail.com>
2023-08-02 21:32:04 +0530

    chart integration completed

---
+++ Routes/Sales.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const data = require("../database");
+const jwt = require("jsonwebtoken");
+const { CategorySales, ProductSold } = require("../Schema/CategoryWiseSales");
+
+router.get("/chart1Data", async (req, res) => {
+  const decodeToken = jwt.verify(req.headers.authorization, "mysecretkey");
+
+  const data = await CategorySales.find({ userid: decodeToken.uid });
+
+  if (data) {
+    res.status(200).send(data);
+  }
+});
+
+router.post("/chart2Data", async (req, res) => {
+  const decodeToken = jwt.verify(req.headers.authorization, "mysecretkey");
+
+  const data = await ProductSold.find({
+    userid: decodeToken.uid,
+    category: req.body.category,
+  });
+  console.log(data);
+  if (data) {
+    res.status(200).send(data);
+  }
+});
+
+module.exports = router;
